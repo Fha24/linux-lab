51b7a0f18baad18d833f47d4e29ba457f9c44f76
Fha24 <narumon.mo.59@ubu.ac.th>
2017-12-01 22:50:35 +0700

    commim linux project

--- narumoncli/narumoncli/cli.py
+++ narumoncli/narumoncli/cli.py
@@ -1,10 +1,25 @@
 import click
+import requests
 
+from PIL import Image
+from StringIO import StringIO
+from bs4 import BeautifulSoup
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """My Tool does one thing, and one thing well."""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+@click.argument('src', nargs=-1)
+@click.argument('dst', nargs=1)
+def main(src, dst):
+    	"""Get Poster Kseries"""
+	name=""	
+	for fn in src:
+        	name+='%s' % (fn)+" "
+	name+='%s'%(dst)
+	url='http://www.kseries.co/{}'.format(name.replace(" ", "-"))
+	html=requests.get(url)
+	beau=BeautifulSoup(html.content,'html.parser')
+	poster=beau.find_all('p',{'class':'className'})
+	beau=BeautifulSoup(str(poster),'html.parser')
+	poster=beau.find_all('img')[0]['src']
+	req=requests.get(str(poster))
+	img=Image.open(StringIO(req.content))
+	img.show()
